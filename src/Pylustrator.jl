f71a3f6fe3cc9f63dfffb1b993ce5184e5a8793f
S J Palmer <samuelpalmer94@gmail.com>
2020-09-30 15:43:10 +0100

    add @pylustrator macro

--- src/Pylustrator.jl
+++ src/Pylustrator.jl
@@ -1,5 +1,26 @@
 module Pylustrator
 
-greet() = print("Hello World!")
+using PyCall, PyPlot, MacroTools
+
+macro pylustrator(expr)
+    # use rmlines so stacktrace returns expected line number
+    return rmlines(quote
+        pylustrator = pyimport("pylustrator")
+        pylustrator.start()
+        # get filename and line position
+        stack_position = stacktrace()[1]
+        filename = abspath(string(stack_position.file))
+        lineno = stack_position.line
+        # @assert filename == @__FILE__
+        ct = pylustrator.change_tracker
+        ct.custom_stack_position = ct.CustomStackPosition(filename, lineno)
+        # wrap generated code in a py"""...""" string
+        ct.custom_prepend = "py\"\"\"from matplotlib import pyplot as plt;"
+        ct.custom_append = "\"\"\""
+        $expr
+    end)
+end
+
+export @pylustrator, @py_str
 
 end # module
